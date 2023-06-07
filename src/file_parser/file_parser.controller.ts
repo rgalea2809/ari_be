28975f76864a50f5f63f2e5a394fc936657331df
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-06-06 19:42:15 -0600

    :sparkles: Adds file convertion from txt to json
    Without encryption

--- src/file_parser/file_parser.controller.ts
+++ src/file_parser/file_parser.controller.ts
@@ -1,24 +1,55 @@
 import {
   Body,
   Controller,
+  HttpStatus,
+  ParseFilePipeBuilder,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileParserService } from './file_parser.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { TxtToJsonDto } from './dto';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import type { Response } from 'express';
 
 @Controller('file-parser')
 export class FileParserController {
   constructor(private fileParserService: FileParserService) {}
 
   @Post('txt-to-json')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uploadedTxtName = 'uploaded-txt';
+          cb(null, `${uploadedTxtName}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  )
   txtToJson(
     @Body() dto: TxtToJsonDto,
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'text/plain', // || 'application/xml' || 'application/json'
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+    @Res({ passthrough: true }) res: Response,
   ) {
+    res.set({
+      'Content-Type': 'application/json',
+      'Content-Disposition': 'attachment;',
+    });
+
     return this.fileParserService.convertTxtToJson(dto, file);
   }
 
--- src/file_parser/file_parser.service.ts
+++ src/file_parser/file_parser.service.ts
@@ -1,14 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  StreamableFile,
+} from '@nestjs/common';
 import { TxtToJsonDto } from './dto';
+import * as fs from 'fs';
 
 @Injectable()
 export class FileParserService {
   async convertTxtToJson(dto: TxtToJsonDto, file: Express.Multer.File) {
-    console.log(file);
-    console.log(dto);
+    try {
+      const data = fs.readFileSync('./uploads/uploaded-txt.txt', 'utf8');
+      const subStrings = data.split(dto.separator);
 
-    return {
-      message: 'TODO',
-    };
+      if (subStrings.length < 7) {
+        throw new BadRequestException('Incorrect input parameters');
+      }
+
+      const json = [
+        {
+          documento: subStrings[0] ?? '',
+          nombres: subStrings[1] ?? '',
+          apellidos: subStrings[2] ?? '',
+          tarjeta: subStrings[3] ?? '',
+          tipo: subStrings[4] ?? '',
+          telefono: subStrings[5] ?? '',
+          poligono: subStrings[6] ?? '',
+        },
+      ];
+
+      // Generate json object
+      var jsonContent = JSON.stringify(json);
+      fs.writeFileSync('./outputs/uploaded-txt.json', jsonContent, 'utf8');
+
+      const file = fs.createReadStream('./outputs/uploaded-txt.json');
+
+      return new StreamableFile(file);
+    } catch (err) {
+      if (err instanceof BadRequestException) {
+        throw err;
+      }
+      throw new InternalServerErrorException('Error reading file');
+    }
   }
 }
