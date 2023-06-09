a59e61e5de8838914560e832ffb14cf424380b13
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-06-09 14:42:28 -0600

    :construction: Adds json to txt logic

--- src/file_parser/file_parser.controller.ts
+++ src/file_parser/file_parser.controller.ts
@@ -76,4 +76,33 @@ export class FileParserController {
   ) {
     return this.fileParserService.convertTxtToXml(dto, file);
   }
+
+  @Post('json-to-txt')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uploadedTxtName = 'uploaded-json';
+          cb(null, `${uploadedTxtName}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  )
+  jsonToTxt(
+    @Body() dto: ConvertionInfoDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'application/json', // || 'application/xml' || 'application/json'
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.fileParserService.convertTxtToJson(dto, file);
+  }
 }
