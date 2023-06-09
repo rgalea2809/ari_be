352d415c92c730d7f8f716c60519f772bf39bfa4
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-06-09 13:00:08 -0600

    :sparkles: Adds encryption to card number

--- src/file_parser/file_parser.service.ts
+++ src/file_parser/file_parser.service.ts
@@ -5,10 +5,15 @@ import {
   StreamableFile,
 } from '@nestjs/common';
 import { TxtToJsonDto } from './dto';
+import { JwtService } from '@nestjs/jwt';
 import * as fs from 'fs';
+import { createCipheriv, createDecipheriv, randomBytes, scrypt } from 'crypto';
+import { promisify } from 'util';
 
 @Injectable()
 export class FileParserService {
+  constructor(private jwt: JwtService) {}
+
   async convertTxtToJson(dto: TxtToJsonDto, file: Express.Multer.File) {
     try {
       const data = fs.readFileSync('./uploads/uploaded-txt.txt', 'utf8');
@@ -18,12 +23,23 @@ export class FileParserService {
         throw new BadRequestException('Incorrect input parameters');
       }
 
+      const encryptedPayload = await this.encryptCardNumber(
+        subStrings[3] ?? '',
+        dto.secret,
+      );
+
+      const tokenifiedCardNumber = await this.tokenifyEncryptedCardNumber(
+        encryptedPayload.encryptedText,
+        encryptedPayload.iv,
+        dto.secret,
+      );
+
       const json = [
         {
           documento: subStrings[0] ?? '',
           nombres: subStrings[1] ?? '',
           apellidos: subStrings[2] ?? '',
-          tarjeta: subStrings[3] ?? '',
+          tarjeta: tokenifiedCardNumber,
           tipo: subStrings[4] ?? '',
           telefono: subStrings[5] ?? '',
           poligono: subStrings[6] ?? '',
@@ -43,5 +59,75 @@ export class FileParserService {
       }
       throw new InternalServerErrorException('Error reading file');
     }
+  }
+
+  async encryptCardNumber(cardNumber: string, secret: string) {
+    const iv = randomBytes(16);
+
+    // The key length is dependent on the algorithm.
+    // In this case for aes256, it is 32 bytes.
+    const key = (await promisify(scrypt)(
+      secret,
+      'secret-salt123',
+      32,
+    )) as Buffer;
+
+    const cipher = createCipheriv('aes-256-ctr', key, iv);
+
+    const encryptedText = Buffer.concat([
+      cipher.update(cardNumber),
+      cipher.final(),
+    ]);
+
+    return {
+      iv: iv.toString('hex'),
+      encryptedText: encryptedText.toString('hex'),
+    };
+  }
+
+  async decryptCardNumber(
+    encryptedCardNumber: string,
+    iv: string,
+    secret: string,
+  ) {
+    // The key length is dependent on the algorithm.
+    // In this case for aes256, it is 32 bytes.
+    const key = (await promisify(scrypt)(
+      secret,
+      'secret-salt123',
+      32,
+    )) as Buffer;
+
+    const decipher = createDecipheriv(
+      'aes-256-ctr',
+      key,
+      Buffer.from(iv, 'hex'),
+    );
+
+    const decryptedText = Buffer.concat([
+      decipher.update(Buffer.from(encryptedCardNumber, 'hex')),
+      decipher.final(),
+    ]);
+
+    return decryptedText.toString();
+  }
+
+  async tokenifyEncryptedCardNumber(
+    encryptedCardNumber: string,
+    iv: string,
+    secret: string,
+  ) {
+    const payLoad = {
+      encryptedCardNumber: encryptedCardNumber,
+      iv: iv,
+    };
+
+    const token = await this.jwt.signAsync(payLoad, {
+      secret: secret,
+    });
+
+    console.log(token);
+
+    return token;
   }
 }
